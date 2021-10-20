0efea3124ccac5bea8b6b60e639d37454fdaf107
Emre Ay <90474810+emreay17@users.noreply.github.com>
2021-10-20 13:33:52 +0300

    added

--- js/04-number-ile-calismak.js
+++ js/04-number-ile-calismak.js
@@ -22,3 +22,40 @@ console.log(
     console.log("Number icine bilgi gonderildi", Number("121"))
 
     // arttirma ve azaltma islemleri:
+
+    let counter = 100
+
+    counter = counter + 1; 
+    counter += 1;
+    counter ++ ;   
+    
+    console.log(counter)
+
+    counter -= 1 ;
+    counter --;
+    console.log(counter)
+
+    let sayi = 100
+    sayi *=10;
+    console.log(sayi)
+    
+    sayi %= 7;  // onluk sistem gosterimi 7 lik sistemde cıktısı 6 oluyor.
+    console.log(sayi)
+
+    console.log( 20 % 3) // çıktısı 3 lük sistemde 2
+
+    //üst alma 
+
+    console.log(5 ** 3) // 5 üzeri 3 çıktısı 125 
+
+    console.log("Asagi Yuvarlama",
+            Math.floor(2.2)) // asagi yuvarlama da 2.2 ciktisi => 2
+
+    console.log("Yukarı yuvarlama",
+            Math.ceil(2.2)) // Yukarı yuvarlama cıktısı => 3
+
+    console.log("yakına yuvarlama",
+            Math.round(2.3)) // cıktısı=> 2
+    console.log("Yakına Yuvarlama",
+            Math.round(2.7)) // cıktısı => 3
+
