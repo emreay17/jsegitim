edabc5582ca731a2e69e0a30297e4f4539aaf3be
Emre Ay <90474810+emreay17@users.noreply.github.com>
2021-10-20 13:07:27 +0300

    first commit

---
+++ js/01-helloworld.js
@@ -0,0 +1,10 @@
+// ********** Merhaba Dunya Ornekleri ********** 
+
+// Dokumana(document) Yazdirma Islemi ile Merhaba Dunya:
+// document.write('Merhaba Dunya');
+
+// Uyari Mesaji (Alert) ile Merhaba Dunya:
+
+alert("Merhaba Dünya")
+
+console.log("MerhabaDünya")
---
+++ js/02-degisken-tanımlama.js
@@ -0,0 +1,30 @@
+// ********** let ve const ile Degisken Tanimlama ********** 
+//var ile degisken tanimlamak:
+var DOMAIN = "emreay@gmail.com"
+console.log(DOMAIN)
+
+// let ile degiskeni bos tanimlamak:
+let server ;
+console.log(server)
+// let ile degiskene bilgi atamak:
+server = "https://google.com"
+console.log(server)
+// let ile degiskene bilgi atayarak tanimlamak:
+let password = "1234"
+console.log(password)
+
+// let ile tanimlanan degiskenin icindeki bilgiyi degistirmek:
+
+let fullName = "Emre Ay"
+console.log(fullName)
+
+// birlestirme veya ekleme islemi
+fullName = fullName + " Yusuf Cinar"
+console.log(fullName)
+
+fullName = "Mahmut Tuncer"
+console.log(fullName)
+
+// const ile degisken tanimlamak:
+const sifre = "1234"
+console.log(password)
