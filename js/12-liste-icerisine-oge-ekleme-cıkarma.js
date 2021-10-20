f00f2c699d326ec28503c6f8ca656e5ad188cc2d
Emre Ay <90474810+emreay17@users.noreply.github.com>
2021-10-20 16:49:24 +0300

    list item added

--- js/12-liste-icerisine-oge-ekleme-cıkarma.js
+++ js/12-liste-icerisine-oge-ekleme-cıkarma.js
@@ -1,3 +1,16 @@
 // liste icerisindeki son elemana ulaşmak ve ya eleman eklemek =>
-let firstChild = document.querySelector("ul#list>li:firstChild")
+let firstChild = document.querySelector("ul#list>li:first-Child")
 firstChild.innerHTML = "değiştirildi"
+
+let lastChild = document.querySelector("ul#list>li:last-child")
+lastChild.innerHTML = "sonunda basardık"
+
+let ulDOM = document.querySelector("ul#list")
+
+let liDOM = document.createElement('li')
+
+liDOM.innerHTML = "nasıl olmus ama ?"
+ulDOM.append(liDOM) // => en sona ekler
+
+liDOM.innerHTML = "bu sefer en basa mı ?"
+ulDOM.prepend(liDOM) // => bu da en basa ekler 
