f629d5c41f71b5c8a7abf4c9bc7ec34bbc9e1c22
Emre Ay <90474810+emreay17@users.noreply.github.com>
2021-10-20 15:31:27 +0300

    dom added

--- js/09-DOM.js
+++ js/09-DOM.js
@@ -1,3 +1,18 @@
 //pathname 
 
 console.log(document.location.pathname)
+
+// head icerisindeki bilgiler için =>
+console.log(document.head)
+
+// body içerisindekileri almak icin 
+console.log(document.body)
+
+// url bilgisini almak icin 
+console.log(document.URL)
+console.log(document.baseURI)
+
+
+// document icerisinde style değiştirmek =>
+document.querySelector("#list").style.color = "red"
+document.querySelector("#list").style.border = "solid black"
