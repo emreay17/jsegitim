15bca6e36c81ef496cb9d714e83aae1443dce1e0
Emre Ay <90474810+emreay17@users.noreply.github.com>
2021-10-29 02:30:12 +0300

    function parametre atamak ve return added

--- js/20-fonksiyonlara-parametre-atamak.js
+++ js/20-fonksiyonlara-parametre-atamak.js
@@ -6,7 +6,30 @@ let firstName = " Emre"
 let lastName = " AY"
 
 function title(firstName="",lastName=""){
-    console.log(`Merhaba ${firstName ? firstName : ""}${lastName ? lastName : ""}`)
+    // console.log(`Merhaba ${firstName ? firstName : ""}${lastName ? lastName : ""}`);
+    console.log(`Merhaba ${firstName} ${lastName}`)
 }
+console.log(firstName)
 title()
+title("parametre")
+
+
+let name = " Emre"
+let surName = " AY"
+function title2(firstName, lastName) {
+let info = `Merhaba ${name} ${surName}`
+return info;}
+
+
+let titleInfo = title2("Ad", "Soyad")
+console.log(titleInfo)
+
+function domIdWriteInfo(id, info){
+    let domObject = querySelector(`#${id}`)
+    domObject.innerHTML = info
+}
+let htmlInfo = `<span style ="color:red> Rengi Kırmızı</span>`
+
+domIdWriteInfo('title', htmlInfo)
+domIdWriteInfo('info', title2("lorem", "Ipsum"))
 
