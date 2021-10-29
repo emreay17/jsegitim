e77c93e4e45916266537516c812eb808da094316
Emre Ay <90474810+emreay17@users.noreply.github.com>
2021-10-29 03:00:21 +0300

    arrow function | added

---
+++ js/22-fom-etkinlikleriyle-calismak.js
@@ -0,0 +1,11 @@
+// DOM Etkinlikleriyle Calismak
+// https://www.w3schools.com/jsref/dom_obj_event.asp
+// https://developer.mozilla.org/en-US/docs/Web/Events
+
+
+let title = document.querySelector("#title")
+title.addEventListener("click", domClick);
+
+function domClick(){
+    console.log("Etkinlik Denetlendi")
+    this.style.color =="red"? this.style.color= "black": this.style.color= "red"}
